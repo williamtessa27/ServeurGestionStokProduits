a44602ad8e34fdd341b921fb3781ecc440464f12
williamtessa27 <williamtessa270794@gmail.com>
2022-12-01 14:24:39 +0000

    first commit

---
+++ src/main/java/com/example/serveurgestionstockproduits/ServeurGestionStockProduitsApplication.java
@@ -0,0 +1,13 @@
+package com.example.serveurgestionstockproduits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServeurGestionStockProduitsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServeurGestionStockProduitsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/serveurgestionstockproduits/ServeurGestionStockProduitsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.serveurgestionstockproduits;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServeurGestionStockProduitsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
